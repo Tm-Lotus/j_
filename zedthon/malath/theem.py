fc814badae9af1505f1fe114b246ee50799fcfb4
Team Lotus <104599580+Tm-Lotus@users.noreply.github.com>
2024-01-16 07:43:39 +0200

    Update theem.py

--- zedthon/malath/theem.py
+++ zedthon/malath/theem.py
@@ -1,14 +1,14 @@
 X = (
-    "𓆩 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𓆪- 𝙏𝙃𝙀𝙈𝙀 ثيمـات](t.me/source_alpop) 𓆪\n\n"
+    "𓆩 𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂 𓆪- 𝚃𝙷𝙴𝙼𝙴 ثيمـات](t.me/l2_2Y) 𓆪\n\n"
     "**⌔∮ قائـمه اوامر الثيمات :**\n\n"
     "⪼ `.ثيمات`\n\n\n"
-    "\n\n 𓆩 [𓆩 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𓆪 - قنـاة السـورس](t.me/source_alpop) 𓆪"
+    "\n\n 𓆩 [𓆩 𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂 𓆪 - قنـاة السـورس](t.me/l2_2Y) 𓆪"
 )
 
 
 ZL = (
-    "𓆩 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𓆪 - 𝙏𝙃𝙀𝙈𝙀 ثيمـات](t.me/source_alpop) 𓆪\n\n"
-    "**⌔∮ قائـمة ثيمـات البوب :**\n"
+    "𓆩 𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂 𓆪 - 𝚃𝙷𝙴𝙼𝙴 ثيمـات](t.me/l2_2Y) 𓆪\n\n"
+    "**⌔∮ قائـمة ثيمـات لوتس :**\n"
     "⪼ `.ن1`\n"
     "⪼ `.ن2`\n"
     "⪼ `.ن3`\n"
@@ -21,10 +21,10 @@ ZL = (
     "⪼ `.ن10`\n"
     "⪼ `.ن11`\n"
     "⪼ `.ن12`\n"
-    "\n\n 𓆩 [𓆩 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𓆪 - قنـاة السـورس](t.me/source_alpop) 𓆪"
+    "\n\n 𓆩 [𓆩 𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂 𓆪 - قنـاة السـورس](t.me/l2_2Y) 𓆪"
 )
 
-FANAN = "𓆩 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏  - 𝙏𝙃𝙀𝙈𝙀 ثيمـات 𓆪"
+FANAN = "𓆩 𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂  - 𝚃𝙷𝙴𝙼𝙴 ثيمـات 𓆪"
 sts_fanan = "https://telegra.ph/file/6da40cf4e58c547f63fb7.jpg"
 sts_fanan2 = "https://telegra.ph/file/e1888c3b3f741365fd020.jpg" 
 sts_fanan3 = "https://telegra.ph/file/c932a7c83cbd4ce874456.jpg"
@@ -51,7 +51,7 @@ sts_fanan23 = "https://telegra.ph/file/359f8fa9d71da3a21d827.jpg"
 sts_fanan24 = "https://telegra.ph/file/6cb3fefec44a39160d926.jpg"
 sts_fanan25 = "https://telegra.ph/file/ed575fb95c274e420e2fe.jpg"
 sts_fanan26 = "https://telegra.ph/file/225ad2b15a2b44b77acf1.jpg"
-THMAT = "𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏  - @SOURCE_ALPOP "
+THMAT = "𝚂𝙾𝚄𝚁𝙲𝙴 𝙻𝙾𝚃𝚄𝚂  - @l2_2Y "
 sts_attheme = "https://t.me/SSSSllv/35"
 sts_attheme2 = "https://t.me/SSSSllv/17" 
 sts_attheme3 = "https://t.me/SSSSllv/27"
